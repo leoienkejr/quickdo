acd0e2830c2593c630389096b718fd6947aec908
leoienkejr <leonardoienkejr@gmail.com>
2021-07-06 22:57:42 -0300

    Create base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,77 @@
+<template>
+    <button class="base-button"
+    :style="styleVars">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'BaseButton',
+    props: {
+        bgColor: {
+            type: String,
+            default: '#EEE'
+        },
+
+        width: {
+            type: String,
+            default: '3rem'
+        },
+
+        height: {
+            type: String,
+            default: '1.5rem'
+        },
+
+        equalSides: {
+            type: Boolean,
+            default: false,
+        },
+
+        borderRadius: {
+            type: Number,
+            default: 8
+        }
+    },
+
+    computed: {
+        styleVars(){
+            return {
+                '--bg-color': this.bgColor,
+                '--width': this.width,
+                '--height': this.equalSides ? this.width : this.height,
+                '--border-radius': `${this.borderRadius}px`
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .base-button {
+        border: unset;
+        cursor: pointer;
+        -webkit-tap-highlight-color: transparent;
+
+        background: var(--bg-color);
+        width: var(--width);
+        height: var(--height);
+        border-radius: var(--border-radius);
+
+    }
+
+    .base-button:focus {
+        outline: 1px solid #999;
+
+    }
+
+    .base-button:active {
+        transform: translateY(1px);
+        filter: blur(0.4px);
+    }
+
+    .base-button:hover {
+        filter: brightness(0.95)
+    }
+</style>
