43db1e46c1982895d0869ed3367096679e5c73ad
leoienkejr <leonardoienkejr@gmail.com>
2021-08-02 22:04:45 -0300

    Create TaskList component

---
+++ src/components/TaskList.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="pb-10 task-list-wrapper"
+    v-if="tasks.length > 0 || !hideEmpty">
+        <div class="task-list-drawer mb-10 flex align-center">
+            <span class="task-list-title">{{title}}</span>
+
+
+            <base-button bg-color="transparent"
+            width="2rem"
+            equal-sides
+            :border-radius="999"
+            class="flex align-center justify-center ml-auto mr-15"
+            @click="showTasks = !showTasks">
+                <i :class="drawerIcon"
+                    :title="showTasks ? 'Hide tasks':'Show tasks'"
+                    :style="{transform: 'scale(1.2)', opacity: 0.8}"></i>
+            </base-button>
+        </div>
+
+        <div v-show="showTasks && tasks.length > 0"
+        class="task-list-tasks">
+            <task-item v-for="task in tasks"
+            :key="task.id"
+            :task="task"
+            class="mb-10" />
+        </div>
+        <div v-show="showTasks && !tasks.length > 0"
+        class="task-list-empty-list-msg-container flex align-center justify-center">
+            <span class="task-list-empty-list-msg">This list is empty.</span>
+        </div>
+
+    </div>
+    
+</template>
+
+
+<script>
+    import BaseButton from '@/components/BaseButton.vue'
+    import TaskItem from '@/components/TaskItem.vue'
+    import {colors} from '@/modules/vars.js'
+
+    export default {
+        components: {
+            TaskItem,
+            BaseButton
+        },
+
+        props: {
+            tasks: {
+                type: Array,
+                default: () => ([
+                    {
+                        id: 1,
+                        title: 'Clean up bedroom',
+                        dueUnixTimeMilliseconds: 1624491050000,
+                        completedAtUnixTimeMilliseconds: 1624476724000,
+                    },
+                    {
+                        id: 2,
+                        title: 'Clean up bedroom',
+                        dueUnixTimeMilliseconds: 1624491050000,
+                        completedAtUnixTimeMilliseconds: 1624476724000,
+                    },
+                    {
+                        id: 3,
+                        title: 'Clean up bedroom',
+                        dueUnixTimeMilliseconds: 1624491050000,
+                        completedAtUnixTimeMilliseconds: 1624476724000,
+                    }
+                ])
+            },
+
+            title: {
+                type: String,
+                default: 'Tasks'
+            },
+
+            accentColor: {
+                type: String,
+                default: colors.primaryBlue,
+                validator: (value) => {
+                    if(!value.length > 0){
+                        console.warn("TaskItem: prop 'accentColor' must not be empty")
+                        return false
+                    } else {
+                        return true
+                    }
+                }
+            },
+
+            hideEmpty: {
+                type: Boolean,
+                default: false
+            }
+        },
+
+        name: 'TaskList',
+
+        data: () => ({
+            showTasks: true,
+        }),
+
+        computed: {
+            drawerIcon(){
+                return this.showTasks ? 'fas fa-chevron-up' : 'fas fa-chevron-down'
+            }
+        },
+
+        methods: {},
+    }
+</script>
+
+
+<style scoped>
+    .task-list-title {
+        font-weight: 600;
+        font-size: 1.5rem;
+    }
+
+    .task-list-drawer {
+        height: 30px;
+    }
+
+    .task-list-wrapper {
+        border-bottom: 1px solid #BBB;
+    }
+
+    .task-list-empty-list-msg-container {
+        height: 80px;
+    }
+
+    .task-list-empty-list-msg {
+        font-size: 0.8rem;
+        opacity: 0.6;
+    }
+</style>
